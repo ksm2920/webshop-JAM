eff8cdaaa462791b19d3e93eeb1e2e411848e57c
Sumi Kim <ksm2920@gmail.com>
2021-03-11 13:44:49 +0100

    add userSchema and validateUserForm function to user model

--- model/user.js
+++ model/user.js
@@ -1,11 +1,27 @@
 const mongoose = require("mongoose");
+const Joi = require("joi");
 
 const userSchema = new mongoose.Schema({
-  
-    name: String
-
+  name: { type:String, required: true, unique: true },
+  email: { type:String, required: true, unique: true },
+  password: { type: String, required: true},
+  role: String,
+  token: String,
+  tokenExpiration: Date
 })
+
+function validateUserForm(User) {
+    const schema = Joi.object({
+        name: Joi.string().min(2).max(30).required(),
+        email: Joi.string().min(5).max(200).required().email(),
+        password: Joi.string().min(8).max(255).required()
+    })
+    return schema.validate(user);
+}
 
 const User = mongoose.model("user", userSchema);
 
-module.exports = User;
+module.exports = {
+    User,
+    validateUserForm
+};
