e0e73658c73028cade84c705f6fe7f12352b5c82
iidalindh <idalindh97@gmail.com>
2021-03-16 10:51:55 +0100

    Adding shoppingcart with functionaltiy and styling according to mockup

--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -1,11 +1,10 @@
-var express = require('express');
-const { loadProducts } = require('../controller/indexController');
+var express = require("express");
+const { loadProducts } = require("../controller/indexController");
+const getUser = require("../middleware/getUser");
 var router = express.Router();
 
-
-
-// GET home page. 
-router.get('/', loadProducts);
+// GET home page.
+router.get("/", getUser, loadProducts);
 
 //router.get('/products/:name', loadProducts);
 
