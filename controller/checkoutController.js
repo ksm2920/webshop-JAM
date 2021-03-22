db65a6ca6e831af229d151b4258defdc82a9f9c1
iidalindh <idalindh97@gmail.com>
2021-03-22 08:14:21 +0100

    Presenting products in basket at checkout-page

--- controller/checkoutController.js
+++ controller/checkoutController.js
@@ -1,34 +1,45 @@
 const { User, validateCheckoutForm } = require("../model/user");
-const {loadProducts} = require("../controller/indexController");
+const { loadProducts } = require("../controller/indexController");
 
 const checkoutRender = async (req, res) => {
+  const userWithCourseData = await User.findOne({
+    _id: req.user.user._id,
+  }).populate("shoppingCart.productId");
+  console.log(userWithCourseData.shoppingCart);
+
   res.render("checkout.ejs", {
     error: "",
-    cartItems: null,
     user: req.user,
+    cartItems: userWithCourseData.shoppingCart,
   });
 };
 
 const checkoutSubmit = async (req, res) => {
   const { error } = validateCheckoutForm(req.body);
   //console.log(error);
-  const {lastname, address, city, zip, phone} = req.body
+  const { lastname, address, city, zip, phone } = req.body;
   const checkoutUserId = req.user.user._id;
 
   if (error) {
     return res.render("checkout.ejs", {
       error: error.details[0].message,
       cartItems: null,
-      user: req.user
+      user: req.user,
     });
   } else {
-   await User.findByIdAndUpdate(
-     checkoutUserId,
-     {lastname:lastname, address:address, city:city, zip:zip, phone:phone},
-     () => {
-      res.clearCookie('jwtToken').redirect('/');
-    }
-   )
+    await User.findByIdAndUpdate(
+      checkoutUserId,
+      {
+        lastname: lastname,
+        address: address,
+        city: city,
+        zip: zip,
+        phone: phone,
+      },
+      () => {
+        res.clearCookie("jwtToken").redirect("/");
+      }
+    );
   }
 };
 
