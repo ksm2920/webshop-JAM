a91dae532c09fc7a855b5ebfc497e9434e40669e
iidalindh <idalindh97@gmail.com>
2021-03-22 10:59:33 +0100

    Implementing payment-functionality with stripe

--- controller/checkoutController.js
+++ controller/checkoutController.js
@@ -1,5 +1,7 @@
 const { User, validateCheckoutForm } = require("../model/user");
 const { loadProducts } = require("../controller/indexController");
+require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 const checkoutRender = async (req, res) => {
   const userWithCourseData = await User.findOne({
@@ -37,13 +39,50 @@ const checkoutSubmit = async (req, res) => {
         phone: phone,
       },
       () => {
-        res.clearCookie("jwtToken").redirect("/");
+        res.redirect("/payment");
       }
     );
   }
 };
 
+const payment = async (req, res) => {
+  const userWithCourseData = await User.findOne({
+    _id: req.user.user._id,
+  }).populate("shoppingCart.productId");
+
+  const session = await stripe.checkout.sessions.create({
+    success_url: "http://localhost:8000/shoppingSuccess",
+    cancel_url: "https://example.com/cancel",
+    payment_method_types: ["card"],
+    line_items: userWithCourseData.shoppingCart.map((productId) => {
+      return {
+        name: productId.productId.name,
+        amount: productId.productId.price * 100,
+        quantity: productId.quantity,
+        currency: "sek",
+      };
+    }),
+    mode: "payment",
+  });
+
+  console.log(session);
+  res.render("payment.ejs", {
+    cartItems: userWithCourseData.shoppingCart,
+    sessionId: session.id,
+  });
+};
+
+const shoppingSuccess = async (req, res) => {
+  const user = await User.findOne({ _id: req.user.user._id });
+  user.shoppingCart = [];
+  user.save();
+
+  res.send("Din varukorg är tom. Vi skickar din beställning inom 3 dagar");
+};
+
 module.exports = {
   checkoutRender,
   checkoutSubmit,
+  payment,
+  shoppingSuccess,
 };
--- routes/checkoutRoute.js
+++ routes/checkoutRoute.js
@@ -1,6 +1,8 @@
 const {
   checkoutRender,
   checkoutSubmit,
+  payment,
+  shoppingSuccess,
 } = require("../controller/checkoutController");
 
 const express = require("express");
@@ -10,5 +12,7 @@ const router = express.Router();
 
 router.get("/checkout", verifyUser, checkoutRender);
 router.post("/checkout", verifyUser, checkoutSubmit);
+router.get("/payment", verifyUser, payment);
+router.get("/shoppingSuccess", verifyUser, shoppingSuccess);
 
 module.exports = router;
