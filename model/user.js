94e87162557a128231710f11efc81af859753109
Deblina-create <deblina4@gmail.com>
2021-03-24 21:48:29 +0100

    resolved few more bugs that were found after merging to develop

--- model/user.js
+++ model/user.js
@@ -13,6 +13,7 @@ const userSchema = new mongoose.Schema({
   city: String,
   zipcode: Number,
   phone: Number,
+  returnUrl: String,
   productList: [
     {
       type: mongoose.Schema.Types.ObjectId,
@@ -69,6 +70,7 @@ function validateLoginForm(user) {
   const schema = Joi.object({
     email: Joi.string().min(5).max(200).required().email(),
     password: Joi.string().min(8).max(255).required(),
+    returnUrl:Joi.string()
   });
   return schema.validate(user);
 }
