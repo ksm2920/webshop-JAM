9a03197cf35be7b994fc7d8c10cd34802e039331
Sumi Kim <ksm2920@gmail.com>
2021-03-24 10:46:34 +0100

    send user data to product page and remove returnUrl after login

--- model/user.js
+++ model/user.js
@@ -69,7 +69,6 @@ function validateLoginForm(user) {
   const schema = Joi.object({
     email: Joi.string().min(5).max(200).required().email(),
     password: Joi.string().min(8).max(255).required(),
-    returnUrl: Joi.string()
   });
   return schema.validate(user);
 }
